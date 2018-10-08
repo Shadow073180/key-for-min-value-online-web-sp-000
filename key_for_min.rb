0273f8ebbcbf7c8bb4b403192f83094a53d35311
David masterson <david73180@gmail.com>
2018-10-08 00:08:25 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,8 +3,9 @@ require 'pry'
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.min_by{|x, y| y
-  return x}
+  name_hash.collect do |x, y|
+  end
+  name_hash[1]
 
 
 end
